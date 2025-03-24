857d11ea0b0f47db9143866fc6a618ed24e26342
A13ssi0 <alessio.palatella@studenti.unipd.it>
2025-03-24 16:42:42 +0100

    Moved general functions in new repo

--- covariances.py
+++ covariances.py
@@ -8,25 +8,6 @@ from pyriemann.utils.mean import mean_riemann
 from pyriemann.utils.base import invsqrtm
 
 from sklearn.decomposition import PCA
-from tqdm import tqdm
-
-
-
-def get_sliding_covariance_trace_normalized(data, wlenght, wshift, substractWindowMean=True, dispProgress=True):
-    nsamples, nchannels = data.shape
-    
-    wstart = np.arange(0, nsamples - wlenght + 1, wshift)
-    wstop = wstart + wlenght
-    
-    nwins = len(wstart)
-    
-    C = np.empty((nwins, nchannels, nchannels))
-    for wId in tqdm (range (nwins), bar_format='{l_bar}{bar:40}{r_bar}', disable=not dispProgress):        
-        cstart = int(wstart[wId])
-        cstop = int(wstop[wId])
-        t_data = data[cstart:cstop, :]
-        C[wId] = get_covariance_matrix_traceNorm(t_data, substractWindowMean)  
-    return C
 
 
 def matrix_to_maxRank(matrix):
@@ -54,35 +35,6 @@ def getrank(data):
         # print(f'Warning: fixing rank computation inconsistency ({tmprank} vs {tmprank2})')
         tmprank2 = min(tmprank, tmprank2)
     return tmprank2
-
-
-def get_covariance_matrix_traceNorm(data, substractWindowMean=True):
-    if substractWindowMean:
-        data -= np.mean(data, axis=0)
-    t_cov = data.T @ data
-    cov =  t_cov  / np.trace(t_cov)
-    return cov
-
-
-# def get_vector_onEvent(df_events, lengthVector, events, clmn_name='typ', onEvent=781):    
-#     vector = np.full(lengthVector, np.nan)
-#     idx_onEvent = df_events[df_events[clmn_name] == onEvent].index
-#     start = df_events.loc[idx_onEvent, 'pos'].values
-#     duration = df_events.loc[idx_onEvent, 'dur'].values
-#     ev_idx = []
-#     for ev in events:
-#         idx = df_events[df_events[clmn_name] == ev].index
-#         ev_idx += idx.tolist()
-#     ev_idx.sort()
-#     ev_type = df_events.loc[ev_idx, clmn_name].values
-#     if len(ev_idx) != len(idx_onEvent):
-#         result1 = np.setdiff1d(ev_idx, idx_onEvent)
-#         print(ev_idx )
-#         print(idx_onEvent)
-#         print(result1)
-#     # for [t_start, t_duration, t_ev_type] in zip(start, duration, ev_type):
-#     #     vector[t_start:(t_start + t_duration)] = t_ev_type
-#     return None#vector
 
 
 def get_riemann_mean_covariance(cov, cueOnFeedbackVector=[], n_iter_max = 300, print_print=True, show_progess=True):
@@ -152,18 +104,9 @@ def get_canonical_bases(vector):
     basis = np.eye(n)
     return basis
 
+
 # # ---------------------- ONLINE ----------------------
-
-def get_covariance_matrix_traceNorm_online(data):
-    data -= np.mean(data, axis=(0,2), keepdims=True)
-    cov = data.transpose((0,2,1)) @ data
-    cov =  cov  / np.trace(cov, axis1=1, axis2=2).reshape(-1,1,1)
-    cov = np.expand_dims(cov, axis=1)
-    return cov
-
 
 def center_covariance_online(covariance, inv_sqrt_mean_cov):
     cov_centered = inv_sqrt_mean_cov @ covariance @ inv_sqrt_mean_cov
-    is_sym_pos_def(cov_centered)
     return cov_centered
-
