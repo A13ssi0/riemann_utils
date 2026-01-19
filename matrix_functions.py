d4d0134a23ce4405567d854e3b633b4e099ad5fa
A13ssi0 <alessio.palatella@studenti.unipd.it>
2026-01-19 14:49:51 +0100

    Minor issues fix

--- matrix_functions.py
+++ matrix_functions.py
@@ -6,6 +6,9 @@ from pyriemann.utils.tangentspace import log_map_riemann
 def metric_riemann(m1, m2, tan_point):
     return np.trace(m1 @ np.linalg.inv(tan_point) @ m2 @ np.linalg.inv(tan_point))
 
+def norm_riemann(m, tan_point):
+    return np.sqrt(metric_riemann(m, m, tan_point))
+
 def angle_between_matrices(m1, m2, tan_point, fullLogMap=True): 
     s1 = log_map_riemann(m1, tan_point, C12=fullLogMap)   
     s2 = log_map_riemann(m2, tan_point, C12=fullLogMap)  
@@ -18,6 +21,50 @@ def angle_between_matrices(m1, m2, tan_point, fullLogMap=True):
     angle = np.arccos(cos_theta)
     
     return angle, cos_theta
+
+
+def compute_runMatrix_angles(run_centroids, tan_point=None):
+    n_band, n_run, n_classes, n_channels, _ = run_centroids.shape
+
+    if tan_point is None:
+        tan_point = np.eye(n_channels)
+
+    matrix_angles = np.zeros((n_band, n_run, n_run, n_classes))
+
+
+    for run_idx1 in range(n_run):
+        print(f"Computing angles for run {run_idx1+1}/{n_run}")
+        for band_idx in range(n_band):
+            for class_idx in range(n_classes):
+                vec1 = run_centroids[band_idx, run_idx1, class_idx]
+                for run_idx2 in range(run_idx1,n_run):
+                    vec2 = run_centroids[band_idx, run_idx2, class_idx]
+
+                    angle, _ = angle_between_matrices(vec1, vec2, tan_point)
+                    matrix_angles[band_idx, run_idx1, run_idx2, class_idx] = angle
+
+    matrix_angles += np.transpose(matrix_angles, (0,2,1,3))
+    return matrix_angles
+
+def compute_runMatrix_distances(run_centroids, mAbsDev_centroids):
+    n_band, n_run, n_classes, n_channels, _ = run_centroids.shape
+
+    matrix_angles = np.zeros((n_band, n_run, n_run, n_classes))
+
+    for run_idx1 in range(n_run):
+        print(f"Computing distances for run {run_idx1+1}/{n_run}")
+        for band_idx in range(n_band):
+            for class_idx in range(n_classes):
+                vec1 = run_centroids[band_idx, run_idx1, class_idx]
+                for run_idx2 in range(run_idx1,n_run):
+                    vec2 = run_centroids[band_idx, run_idx2, class_idx]
+
+                    angle = distance_riemann(vec1, vec2)
+                    angle /= (mAbsDev_centroids[band_idx, run_idx1, class_idx] + mAbsDev_centroids[band_idx, run_idx2, class_idx])/2
+                    matrix_angles[band_idx, run_idx1, run_idx2, class_idx] = angle
+
+    matrix_angles += np.transpose(matrix_angles, (0,2,1,3))
+    return matrix_angles
 
 def matrix_std(matrices, center_point):
     # matrices: ... x n_matrices x n x n
